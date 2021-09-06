76dcd1244b4c822916dfe9b30caeefc5cd66b8a0
lara <lara@vehicle4change.org>
2021-09-06 15:54:59 -0400

    updated index route for manifest

--- server.js
+++ server.js
@@ -15,15 +15,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/budget-tracker-osu',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  },
-);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
 
 // routes
 app.use(require("./routes/api.js"));
