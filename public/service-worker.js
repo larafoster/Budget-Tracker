87a662625ab16b9088050425e0b7df0563d53890
lara <lara@vehicle4change.org>
2021-09-07 19:10:00 -0400

    revised sw

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,51 +1,76 @@
 const FILES_TO_CACHE = [
-    "/",
-    "manifest.json",
-    "index.html",
-    "index.js",
-    "styles.css",
-    "db.js",
-    "icons/icon-192x192.png",
-    "icons/icon-512x512.png",
-    "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
-    "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
+  '/',
+  'manifest.json',
+  'index.html',
+  'index.js',
+  'styles.css',
+  'db.js',
+  'icons/icon-192x192.png',
+  'icons/icon-512x512.png',
+  'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+  'https://cdn.jsdelivr.net/npm/chart.js@2.8.0',
 ];
 
 const PRECACHE = 'precache-v1';
-const RUNTIME = 'runtime';
+const RUNTIME_CACHE = 'runtime-cache';
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
+self.addEventListener ('install', event => {
+  event.waitUntil (
     caches
-      .open(PRECACHE)
-      .then((cache) => cache.addAll(FILES_TO_CACHE))
-      .then(self.skipWaiting())
+      .open (PRECACHE)
+      .then (cache => cache.addAll (FILES_TO_CACHE))
+      .then (self.skipWaiting ())
   );
 });
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        fetch(event.request)
-          .catch(() => {
-            return caches.open(PRECACHE)
-              .then((cache) => {
-                return cache.match(event.request)
-              })
+
+self.addEventListener ('activate', event => {
+  event.waitUntil (
+    caches
+      .keys ()
+      .then (keyList => {
+        return Promise.all (
+          keyList.map (key => {
+            if (key !== PRECACHE) {
+              console.log ('[ServiceWorker] Removing old cache', key);
+              return caches.delete (key);
+            }
           })
-    )
-})
-// The activate handler takes care of cleaning up old caches.
-self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys()
-          .then((keyList) => {
-            return Promise.all(keyList.map((key) => {
-              if (key !== PRECACHE) {
-                console.log('[ServiceWorker] Removing old cache', key)
-                return caches.delete(key)
-              }
-            }))
-          })
-          .then(() => self.clients.claim())
-    )
+        );
+      })
+      .then (() => self.clients.claim ())
+  );
 });
 
+// handle runtime_CACHE GET requests for data from /api routes
+self.addEventListener ('fetch', event => {
+  if (event.request.url.includes ('/api/')) {
+    event.respondWith (
+      caches
+        .open (RUNTIME_CACHE)
+        .then (cache => {
+          return fetch (event.request)
+            .then (response => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put (event.request.url, response.clone ());
+              }
+
+              return response;
+            })
+            .catch (err => {
+              return cache.match (event.request);
+            });
+        })
+        .catch (err => console.log (err))
+    );
+
+    return;
+  }
+
+  // request is not in cache. make network request and cache the response
+  event.respondWith (
+    caches.match (event.request).then (function (response) {
+      return response || fetch (event.request);
+    })
+  );
+});
