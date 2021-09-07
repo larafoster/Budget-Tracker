e204b0432c244fc25eb754d53d52048325036325
lara <lara@vehicle4change.org>
2021-09-07 19:49:46 -0400

    revised sw

--- public/service-worker.js
+++ public/service-worker.js
@@ -11,66 +11,63 @@ const FILES_TO_CACHE = [
   'https://cdn.jsdelivr.net/npm/chart.js@2.8.0',
 ];
 
-const PRECACHE = 'precache-v1';
-const RUNTIME_CACHE = 'runtime-cache';
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
 
 self.addEventListener ('install', event => {
-  event.waitUntil (
-    caches
-      .open (PRECACHE)
-      .then (cache => cache.addAll (FILES_TO_CACHE))
-      .then (self.skipWaiting ())
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log("Your files were pre-cached successfully!");
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+
+    self.skipWaiting();
 });
 
 self.addEventListener ('activate', event => {
-  event.waitUntil (
-    caches
-      .keys ()
-      .then (keyList => {
-        return Promise.all (
-          keyList.map (key => {
-            if (key !== PRECACHE) {
-              console.log ('[ServiceWorker] Removing old cache', key);
-              return caches.delete (key);
-            }
-          })
-        );
-      })
-      .then (() => self.clients.claim ())
-  );
-});
-
-// handle runtime_CACHE GET requests for data from /api routes
-self.addEventListener ('fetch', event => {
-  if (event.request.url.includes ('/api/')) {
-    event.respondWith (
-      caches
-        .open (RUNTIME_CACHE)
-        .then (cache => {
-          return fetch (event.request)
-            .then (response => {
-              // If the response was good, clone it and store it in the cache.
-              if (response.status === 200) {
-                cache.put (event.request.url, response.clone ());
-              }
-
-              return response;
-            })
-            .catch (err => {
-              return cache.match (event.request);
-            });
+    event.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log("Removing old cache data", key);
+                        return caches.delete(key);
+                    }
+                })
+            );
         })
-        .catch (err => console.log (err))
     );
 
-    return;
-  }
+    self.clients.claim();
+});
 
-  // request is not in cache. make network request and cache the response
-  event.respondWith (
-    caches.match (event.request).then (function (response) {
-      return response || fetch (event.request);
-    })
-  );
+self.addEventListener ('fetch', event => {
+    // cache successful requests to the API
+    if (event.request.url.includes("/api/")) {
+        event.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(event.request)
+                    .then(response => {
+
+                        if (response.status === 200) {
+                            cache.put(event.request.url, response.clone());
+                        }
+
+                        return response;
+                    })
+                    .catch(err => {
+                        return cache.match(event.request);
+                    });
+            }).catch(err => console.log(err))
+        );
+
+        return;
+    }
+
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
+    );
 });
